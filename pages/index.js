6f80984a2e8b3857f69f229d6e4dc7f7fc0275c2
patronum1 <75047825+patronum1@users.noreply.github.com>
2022-11-13 05:00:13 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="encom node.js project" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Засвар хийгдэж байна</code>
         </p>
       </main>
 
